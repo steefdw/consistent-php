1c9207680ff423e0e63f4210a5bf5365d8cba436
steefdw <beatniak@gmail.com>
2016-02-07 16:54:16 +0100

    Removed example from phpunit docs

--- tests/BasicArrTest.php
+++ tests/BasicArrTest.php
@@ -57,22 +57,4 @@ class BasicArrTest extends PHPUnit_Framework_TestCase
         ];
     }
 
-    /**
-     * @dataProvider additionProvider
-     /
-    public function testAdd($a, $b, $expected)
-    {
-        true;//$this->assertEquals($expected, $a + $b);
-    }
-
-    public function additionProvider()
-    {
-        return array(
-            'adding zeros'  => array(0, 0, 0),
-            'zero plus one' => array(0, 1, 1),
-            'one plus zero' => array(1, 0, 1),
-            'one plus one'  => array(1, 1, 3)
-        );
-    }*/
-
 }
